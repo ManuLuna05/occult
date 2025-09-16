8536e027375378597fc0457e08ecaeff625a5682
Manuel Luna <lunala.manuel@gmail.com>
2025-09-16 20:45:05 +0200

    cambios app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
-  <title>@yield('title','Occult F | Web Oficial')</title>
+  <title>@yield('title','Occult FC | Web Oficial')</title>
 
   {{-- Favicons / PWA desde public/images --}}
   <link rel="icon" type="image/png" href="{{ asset('images/escudos/logo.png') }}">
@@ -15,15 +15,15 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.min.css">
 
   {{-- CSS planos desde public/css --}}
-  <link rel="stylesheet" href="{{ asset('css/base.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/header.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/hero.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/squad.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/player-card.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/contenido.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/clasificacion.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/calendario.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/base.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/header.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/footer.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/hero.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/squad.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/player-card.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/contenido.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/clasificacion.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/calendario.css') }}">
 </head>
 <body>
   @include('partials.header')
@@ -35,6 +35,6 @@
   @include('partials.footer')
 
   {{-- JS plano desde public/js --}}
-  <script src="{{ asset('js/carrusel.js') }}" defer></script>
+  <script src="{{ asset('/js/carrusel.js') }}" defer></script>
 </body>
 </html>
